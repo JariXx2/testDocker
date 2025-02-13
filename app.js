c02c21c1d01d6d2a3595e34da539be00205be46f
JariXx2 <korelskiy_2000@mail.ru>
2025-02-13 13:03:19 +0300

    test connect postgresql

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
+const {Client} = require("pg");
 
-app.get('/',(req,res)=>{
-    res.send("Hello World!")
+const client = new Client({
+    user: process.env.DB_USER || "postgres",
+    password: process.env.DB_PASSWORD || "postgres",
+    host: process.env.DB_HOST || "localhost",
+    port: process.env.DB_PORT || 5432,
+    database: process.env.DB_NAME || "postgres",
 });
 
-app.listen(port,()=>{
+app.get("/", (req, res) => {
+    client
+    .connect()
+    .then(() => console.log("Connected to PostgreSQL"))
+    .catch((err) => console.error("Connection error", err.stack));
+    res.send("Hello World!");
+});
+
+app.listen(port, () => {
     console.log(`App listen at http://localhost:${port}`);
 });
